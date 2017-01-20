e915f6e3ceaf62156071a80cf69675f4ad1bcad0
henioStraszny <jacek.styrylski@gmail.com>
2017-01-20 17:59:51 +0100

    Initial version

---
+++ cc-pok-server-web-api/src/cc-pok-server-web-api/Model/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace cc_pok_server_web_api.Model
+{
+    public class Employee
+    {
+        public Guid EmployeID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+                //optional
+        public string Email { get; set; }
+        //optional
+        public string Phone { get; set; }
+
+    }
+}
---
+++ cc-pok-server-web-api/src/cc-pok-server-web-api/Model/IEmployeeRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cc_pok_server_web_api.Model
+{
+    public interface IEmployeeRepository
+    {
+        void Add(Employee employee);
+        IEnumerable<Employee> GetAll();
+        Employee Find(Guid key);
+        Employee Remove(Guid key);
+        void Update(Employee employee);
+    }
+}
